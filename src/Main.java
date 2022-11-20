2907be2a0b6b224e08cef3399ea964184f7e081a
Itxvibhu12 <vik6026717961@gmail.com>
2022-11-20 14:49:36 +0530

    initial version

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("my name is khan");
+        System.out.println("hello bby aap kaise ho ");
+
+    }
+}
